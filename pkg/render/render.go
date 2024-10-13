47a13d4beb70c5c43c8be6bc51ab8c6a7f374dc2
IndexCoder <programmer.kg@mail.ru>
2024-10-13 21:37:56 +0600

    init projects

---
+++ pkg/render/render.go
@@ -0,0 +1,82 @@
+package render
+
+import (
+	"bytes"
+	"github.com/indexcoder/bookings/pkg/config"
+	"github.com/indexcoder/bookings/pkg/models"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+var functions = template.FuncMap{}
+var app *config.AppConfig
+
+func NewTemplate(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+
+	return td
+}
+
+func Template(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+
+	var tc map[string]*template.Template
+
+	if app.UseCache {
+		tc = app.TemplateCache
+	} else {
+		tc, _ = TemplateCache()
+	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatalf("Could not get template from template cache")
+	}
+
+	buf := new(bytes.Buffer)
+
+	td = AddDefaultData(td)
+
+	_ = t.Execute(buf, td)
+
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		log.Printf("Error writing template to response: %v", err)
+	}
+}
+
+func TemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.html")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.html")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.html")
+			if err != nil {
+				return myCache, err
+			}
+		}
+		myCache[name] = ts
+	}
+	return myCache, nil
+}
